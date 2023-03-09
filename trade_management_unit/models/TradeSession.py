055491262c369e91b0a1819926f0f2ddd4938820
root <thinkjoshi@gmail.com>
2023-03-09 18:04:09 +0000

    complete trade_session model

--- trade_management_unit/models/TradeSession.py
+++ trade_management_unit/models/TradeSession.py
@@ -7,7 +7,9 @@ class TradeSession(models.Model):
   
     id = models.CharField(auto_created=True,primary_key=True,blank=False,max_length=64,default="") 
     started_at = models.DateTimeField(blank=False)
-    closed_at =  models.DateTimeField()
-    net_profit = models.FloatField()
-    is_closed = models.BooleanField(default=False) 
+    closed_at =  models.DateTimeField(blank=True,null=True)
+    net_profit = models.FloatField(blank=True,null=True)
+    is_closed = models.BooleanField(default=False)
+    scanning_alogo_id = models.IntegerField(blank=False,default=1)
+
 
