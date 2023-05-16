7694dc49d480c08ccf600141c0a4bb661571249b
naresh_joshi01 <thinkjoshi@gmail.com>
2023-05-16 23:38:18 +0530

    update requirements.txt

---
+++ sample.js
@@ -0,0 +1,59 @@
+sample_live_data = 
+{
+    "instrument_token": 779521,
+    "last_price": 1219.9,
+    "last_quantity": 2,
+    "average_price": 1208.4,
+    "volume": 10215,
+    "buy_quantity": 2826,
+    "sell_quantity": 2058,
+    "ohlc": {
+        "open": 1202.2,
+        "high": 1231.45,
+        "low": 1202.2,
+        "close": 1224.3
+    },
+    "change": 0.98,
+    "last_trade_time": "2021-10-22 15:29:59",
+    "oi": 4220,
+    "oi_day_high": 4342,
+    "oi_day_low": 4220,
+    "depth": {
+        "buy": [
+            {
+                "quantity": 1123,
+                "price": 1219.55,
+                "orders": 36
+            },
+            {
+                "quantity": 171,
+                "price": 1219.5,
+                "orders": 7
+            },
+            {
+                "quantity": 26,
+                "price": 1219.45,
+                "orders": 1
+            },
+            ...
+        ],
+        "sell": [
+            {
+                "quantity": 406,
+                "price": 1219.95,
+                "orders": 22
+            },
+            {
+                "quantity": 1,
+                "price": 1220,
+                "orders": 1
+            },
+            {
+                "quantity": 48,
+                "price": 1220.1,
+                "orders": 3
+            },
+            ...
+        ]
+    }
+}
