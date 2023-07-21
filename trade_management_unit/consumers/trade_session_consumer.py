832262f243ab6cecda63d41ad3c6f29e8a6608ae
Naresh Joshi <thinkjoshi@gmail.com>
2023-07-21 14:42:19 +0530

    Websocket works now

--- trade_management_unit/consumers/trade_session_consumer.py
+++ trade_management_unit/consumers/trade_session_consumer.py
@@ -1,23 +1,24 @@
 from channels.generic.websocket import WebsocketConsumer
-# from asgiref.sync import asyn_to_sync
+from asgiref.sync import async_to_sync
 import json
 class TradeSession(WebsocketConsumer):
-    async def connect(self):
+    def connect(self):
         print("In Consumer Now")
         self.room_name = "test_consumer"
         self.room_group_name = "test_consumer_group"
-        async_to_sync(self.channel_layer.group.add)(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_name, self.room_group_name
         )
         self.accept()
-        self.send(text_data = json.dumps({"status":"connected"}))
+        self.send(text_data = json.dumps({"status":"connected from channels now  "}))
 
-    async def disconnect(self, close_code):
-        pass
+    def disconnect(self, *args , **kwargs):
+        print("Disconnected Now")
 
-    async def receive(self, text_data=None, bytes_data=None):
+
+    def receive(self, text_data=None, bytes_data=None):
         # Handle received market update data and update UI or trigger trade actions.
-        pass
+        self.send(text_data = json.dumps({"recieved":text_data}))
 
     # async def send(self, text_data=None, bytes_data=None):
     #     # Send data to the client.
