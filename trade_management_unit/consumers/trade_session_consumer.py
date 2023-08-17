aaafdc73bc2020b5bbd0cda21695ba716cb5ed82
Naresh Joshi <thinkjoshi@gmail.com>
2023-08-17 18:21:54 +0530

    use async consumer

--- trade_management_unit/consumers/trade_session_consumer.py
+++ trade_management_unit/consumers/trade_session_consumer.py
@@ -1,24 +1,25 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer,AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 import json
-class TradeSession(WebsocketConsumer):
-    def connect(self):
+class TradeSession(AsyncWebsocketConsumer):
+    async def connect(self):
         print("In Consumer Now")
         self.room_name = "test_consumer"
         self.room_group_name = "test_consumer_group"
-        async_to_sync(self.channel_layer.group_add)(
+        await (self.channel_layer.group_add)(
             self.room_name, self.room_group_name
         )
-        self.accept()
-        self.send(text_data = json.dumps({"status":"connected from channels now  "}))
+        await self.accept()
+        await self.send(text_data = json.dumps({"status":"connected from channels now  "}))
 
-    def disconnect(self, *args , **kwargs):
+    async def disconnect(self, *args , **kwargs):
         print("Disconnected Now")
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         # Handle received market update data and update UI or trigger trade actions.
-        self.send(text_data = json.dumps({"recieved":text_data}))
+        await self.send(text_data = json.dumps({"recieved":text_data}))
 
     # async def send(self, text_data=None, bytes_data=None):
     #     # Send data to the client.
