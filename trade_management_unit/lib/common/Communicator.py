b6f77f3f8eaae412b2cf8eb4d8dcf5167e549409
Naresh Joshi <thinkjoshi@gmail.com>
2023-09-01 16:10:09 +0530

    Modify communicator to send chain of data in a loop

--- trade_management_unit/lib/common/Communicator.py
+++ trade_management_unit/lib/common/Communicator.py
@@ -1,21 +1,32 @@
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from django.core import serializers
 import asyncio
+import logging
+import json
+
 
 
 class Communicator:
     def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        self.loop = asyncio.get_event_loop()
         pass
     
     def send_data_to_channel_layer(self, data, group_name):
-        group_name = "trade_group"
-        print("In liberary sending data")
-        channel_layer = get_channel_layer()
-        print("Sendiong now to group",group_name)
-        async_to_sync(channel_layer.group_send)(group_name, {
-            "type": "send.data",
-            "data": data,
-        })
+        try:
+
+            print("In liberary sending data")
+            # print("Ticks {}".format(data))
+            channel_layer = get_channel_layer()
+            print("Sending now to group",group_name)
+            coro = (channel_layer.group_send)(group_name, {
+                "type": "send.data",
+                "data": data,
+            })
+            future = asyncio.run_coroutine_threadsafe(coro, self.loop)
+        except Exception as e:
+            print("Eror Occoured",e)
         print("Message sent to group")
 
 
