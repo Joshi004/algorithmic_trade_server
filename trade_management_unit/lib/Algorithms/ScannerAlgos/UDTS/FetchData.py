0e6476c399bd9303a9641984e73dcfe399df1d88
Naresh Joshi <thinkjoshi@gmail.com>
2023-09-16 16:49:12 +0530

    update fetch class

--- trade_management_unit/lib/Algorithms/ScannerAlgos/UDTS/FetchData.py
+++ trade_management_unit/lib/Algorithms/ScannerAlgos/UDTS/FetchData.py
@@ -1,21 +1,31 @@
 import pandas as pd
 from datetime import datetime, timedelta
+import requests
 
 class FetchData:
     def __init__(self):
         pass
 
     def sanitize_data(self, data):
-        # Remove any leading or trailing spaces from column names
+         # Remove leading and trailing spaces from column names
+        print(data.columns)
         data.columns = data.columns.str.strip()
+        # Convert column names to snake case
+        data = data.rename(columns={
+        'Open Price': 'open',
+        'Close Price': 'close',
+        'High Price': 'high',
+        'Low Price': 'low'
+    })
+        data.columns = data.columns.str.lower().str.replace(' ', '_').str.replace(".","")
 
         # Convert all string columns to uppercase to ensure consistency
         string_columns = data.select_dtypes(include='object').columns
         for col in string_columns:
             data[col] = data[col].str.upper()
-
+        print(data.columns)
         # Sanitize date columns
-        date_columns = ['Date']
+        date_columns = ['date']
         for col in date_columns:
             data[col] = pd.to_datetime(data[col], format='%d-%b-%Y', errors='coerce')
 
@@ -25,8 +35,9 @@ class FetchData:
 
     def fetch_candle_data(self, symbol, start_date, end_date,interval=0):
         # Convert start and end dates to datetime objects
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        if(type(start_date) == type(end_date) == "str"):
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
 
         # Read data from CSV file
         file_path = f'/Users/nareshjoshi/Documents/personal_workspace/ats_aplication/ats_server/data/{symbol}.csv'
@@ -36,8 +47,29 @@ class FetchData:
         data = self.sanitize_data(data)
 
         # Filter rows based on start and end dates
-        mask = (data['Date'] >= start_date) & (data['Date'] <= end_date)
+        mask = (data['date'] >= start_date) & (data['date'] <= end_date)
         data = data.loc[mask]
-
         return data.to_dict('records')
 
+    def fetch_from_nse(self, symbol, start_date, end_date):
+        # Convert dates to the required format
+        start_date = start_date.strftime('%d-%m-%Y')
+        end_date = end_date.strftime('%d-%m-%Y')
+
+        # Capitalize the symbol
+        symbol = symbol.upper()
+
+        # Construct the URL
+        url = f"https://www.nseindia.com/api/historical/securityArchives?from={start_date}&to={end_date}&symbol={symbol}&dataType=priceVolumeDeliverable&series=ALL"
+        
+        # Fetch data from NSE
+        print("Gettinh historical data now")
+        response =  requests.get(url)
+        print("response",response)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Failed to fetch data from NSE. Status code: {response.status_code}")
+            return None
