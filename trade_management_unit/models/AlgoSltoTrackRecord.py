27d8997a76e01f82299c9b16e304af8439197d01
naresh_joshi01 <673ifbhi9t9u70hxd8o1162y19co10@bots.bitbucket.org>
2023-09-29 19:44:09 +0530

    ATS-8-Change algo_slto_track_records

--- trade_management_unit/models/AlgoSltoTrackRecord.py
+++ trade_management_unit/models/AlgoSltoTrackRecord.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django_mysql.models import EnumField
+from trade_management_unit.Constants.TmuConstants import PriceZone  # assuming constants.py is in the same directory
 
 class Slto(models.Model):
     class Meta:
@@ -8,4 +10,10 @@ class Slto(models.Model):
     recorded_at = models.DateTimeField(auto_now_add=True)
     market_price = models.DecimalField(max_digits=10, decimal_places=2)
     trade = models.ForeignKey('Trade', on_delete=models.CASCADE)
-    order = models.ForeignKey('Order', on_delete=models.CASCADE, null=True, blank=True)
+
+    # New fields
+    instrument = models.ForeignKey('Instrument', on_delete=models.CASCADE)
+    PRICE_ZONE_CHOICES = [(tag.name, tag.value) for tag in PriceZone]
+    existing_price_zone = EnumField(choices=PRICE_ZONE_CHOICES, default=PriceZone.RANGE.value)
+    next_price_zone = EnumField(choices=PRICE_ZONE_CHOICES, default=PriceZone.RANGE.value)
+    existing_price_zone_time = models.IntegerField(null=True)
