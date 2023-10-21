4c11fd659a67167263d4e8eaebc717634e0575fe
naresh_joshi01 <673ifbhi9t9u70hxd8o1162y19co10@bots.bitbucket.org>
2023-10-21 15:46:46 +0530

    ATS-9-create models fro Dumy User & User config

---
+++ trade_management_unit/models/DummyUser.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+class DummyUser(models.Model):    
+    id = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=200, db_index=True)  # Index added
+    initial_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    current_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    last_balance_refreshed_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "dummy_users"
+
+    @classmethod
+    def get_attribute(cls, username, attribute):
+        try:
+            user = cls.objects.get(dummy_user_name=username)
+            return getattr(user, attribute, None)
+        except cls.DoesNotExist:
+            return None
---
+++ trade_management_unit/models/UserConfiguration.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class UserConfiguration(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(unique=True)
+    risk_appetite = models.FloatField(default=5, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    risk_reward_ratio = models.FloatField(default=0.5)
+
+    class Meta:
+        db_table = "user_configurations"
+
+
+    @classmethod
+    def get_attribute(cls, user_id, attribute):
+        try:
+            user_config = cls.objects.get(user_id=user_id)
+            return getattr(user_config, attribute, None)
+        except cls.DoesNotExist:
+            return None
