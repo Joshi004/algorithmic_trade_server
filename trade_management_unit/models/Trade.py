6475e4e86102e2d872173ea5628d3d8b526d9486
naresh_joshi01 <673ifbhi9t9u70hxd8o1162y19co10@bots.bitbucket.org>
2023-10-26 20:55:06 +0530

    ATS-9-Modify trade model

--- trade_management_unit/models/Trade.py
+++ trade_management_unit/models/Trade.py
@@ -2,6 +2,8 @@ from django.db import models
 from django_mysql.models import EnumField
 from datetime import datetime
 from django.db.models import Q
+from trade_management_unit.models.Instrument import Instrument
+from trade_management_unit.Constants.TmuConstants import *
 class Trade(models.Model):
     class Meta:
         db_table = "trades"
@@ -9,7 +11,7 @@ class Trade(models.Model):
     id = models.CharField(auto_created=True, primary_key=True, blank=False, max_length=64, default="") 
     is_active = models.BooleanField(default=True)
     started_at = models.DateTimeField(blank=False)
-    closed_at = models.DateTimeField()
+    closed_at = models.DateTimeField(blank=True, null=True)
     instrument = models.ForeignKey("Instrument", verbose_name="Ordered Instrument", on_delete=models.CASCADE)   
     trade_session = models.ForeignKey("TradeSession", verbose_name="Trade Session", on_delete=models.CASCADE, default=None)   
     net_profit = models.FloatField(blank=True, null=True)
@@ -20,13 +22,22 @@ class Trade(models.Model):
     max_price = models.FloatField(blank=True, null=True)
     min_price = models.FloatField(blank=True, null=True)
 
+    @classmethod
+    def __get_instrument_id__(cls, symbol):
+        breakpoint()
+        instrument = Instrument.objects.get(trading_symbol=symbol,exchange=DEFAULT_EXCHANGE)
+        return instrument.id
+     
+    
 
     @classmethod
     def fetch_or_initiate_trade(cls, trading_symbol, action, trade_session_id, user_id, dummy):
+        instrument_id = cls.__get_instrument_id__(trading_symbol)
+        breakpoint()
         try:
             # Try to find an existing active trade with the same parameters
             trade = cls.objects.get(
-                instrument_id=trading_symbol,
+                instrument_id=instrument_id,
                 trade_session_id=trade_session_id,
                 user_id=user_id,
                 is_active=True
@@ -37,7 +48,7 @@ class Trade(models.Model):
                 is_active=True,
                 started_at=datetime.now(),
                 closed_at=None,
-                instrument_id=trading_symbol,
+                instrument_id=instrument_id,
                 trade_session_id=trade_session_id,
                 view='long' if action == 'buy' else 'short',
                 user_id=user_id,
