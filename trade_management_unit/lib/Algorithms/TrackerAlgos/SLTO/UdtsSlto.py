7c5a67229bed4fe47fa3f81994a00d88bb76ccf5
naresh_joshi01 <673ifbhi9t9u70hxd8o1162y19co10@bots.bitbucket.org>
2023-10-31 17:08:23 +0530

    ATS-9-Put Process Tracker actions in tracker algo file

--- trade_management_unit/lib/Algorithms/TrackerAlgos/SLTO/UdtsSlto.py
+++ trade_management_unit/lib/Algorithms/TrackerAlgos/SLTO/UdtsSlto.py
@@ -1,5 +1,10 @@
 from trade_management_unit.lib.Indicators.SLTO.SLTO import SLTO
 from trade_management_unit.lib.Algorithms.TrackerAlgos.TrackerAlgoMeta import TrackerAlgoMeta
+from trade_management_unit.models import Trade
+from trade_management_unit.models.Order import  Order
+from trade_management_unit.lib.Portfolio.Portfolio import Portfolio
+from trade_management_unit.Constants.TmuConstants import  *
+from trade_management_unit.lib.TradeSession.RiskManager import RiskManager
 class UdtsSlto(metaclass=TrackerAlgoMeta):
     def __init__(self,trading_frequency,scanning_algorithm_name):
         self.trading_frequency = trading_frequency
@@ -20,3 +25,33 @@ class UdtsSlto(metaclass=TrackerAlgoMeta):
     
     def register_trade_session(self,trade_session_obj):
         self.trade_sessions[str(trade_session_obj)] = trade_session_obj
+
+
+    def __process_tracker_actions__(self,trade_session,instrument,trade_session_id,user_id,dummy):
+        trading_symbol = instrument["trading_symbol"]
+        action = instrument["required_action"]
+        if action:
+            instrument_id = instrument["instrument_id"]
+            market_price = instrument["market_data"]["market_price"]
+            trade_id = Trade.fetch_trade(instrument_id,trade_session_id,user_id,dummy).id
+            quantity = self.__get_square_off_quantity__(instrument_id,trade_session_id,user_id,trade_id,dummy)
+            if not dummy:
+                order_params = {"trading_symbol":trading_symbol,
+                                "exchange" : DEFAULT_EXCHANGE ,
+                                "transaction_type" : action ,
+                                "order_type" : "MARKET" ,
+                                "quantity" :quantity ,
+                                "product" : "MIS" ,
+                                "validity" : "DAY"}
+                kite_order_id = Portfolio.initiate_order(order_params)
+
+            frictional_losses = RiskManager().get_frictional_losses(TRADE_TYPE["intraday"],market_price, quantity, action == "BUY")
+            order = Order.initiate_order(action, instrument_id, trade_id, dummy, kite_order_id, frictional_losses, user_id, quantity)
+            return trade_id
+        return None
+
+    def __get_square_off_quantity__(self,instrument_id,trade_session_id,user_id,trade_id,dummy):
+        order = Order.fetch_order(instrument_id, trade_id,dummy,user_id)
+        existing_quantity = order.quantity
+        return existing_quantity
+
