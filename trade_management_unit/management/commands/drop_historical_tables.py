51d6d734d7e6a4e23f35e8cd808ce20dc235a031
naresh_joshi01 <673ifbhi9t9u70hxd8o1162y19co10@bots.bitbucket.org>
2023-11-19 01:53:26 +0530

    ATS-24-Create script to delete all historical data 01

--- trade_management_unit/management/commands/drop_historical_tables.py
+++ trade_management_unit/management/commands/drop_historical_tables.py
@@ -8,10 +8,8 @@ class Command(BaseCommand):
         with connection.cursor() as cursor:
             # Execute the SHOW TABLES command
             cursor.execute("SHOW TABLES LIKE 'hd__%'")
-
             # Fetch all table names
             tables = cursor.fetchall()
-
             # Loop through all tables
             for table in tables:
                 # Construct the DROP TABLE query
